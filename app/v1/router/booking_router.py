2b0c27736c1df27333e4867756002ca0124c0a69
sergio2448 <84557725+sergio2448@users.noreply.github.com>
2022-11-25 18:58:03 -0500

    add: crud booking

---
+++ app/v1/router/booking_router.py
@@ -0,0 +1,82 @@
+from fastapi import APIRouter, Depends, Body, status, Path
+
+from typing import List
+
+from app.v1.schema import booking_schema
+from app.v1.service import booking_service
+from app.v1.utils.db import get_db
+
+
+router = APIRouter(prefix="/api/v1")
+
+@router.post(
+    "/booking",
+    tags=["bookings"],
+    status_code=status.HTTP_201_CREATED,
+    response_model=booking_schema.Booking,
+    dependencies=[Depends(get_db)]
+)
+def add_booking(booking: booking_schema.BookingBase = Body(...)):
+    return booking_service.add_booking(booking)
+
+@router.get(
+    "/bookings",
+    tags=["bookings"],
+    status_code=status.HTTP_200_OK,
+    response_model=List[booking_schema.Booking],
+    dependencies=[Depends(get_db)]
+)
+def get_bookings():
+    return booking_service.get_bookings()
+
+
+@router.get(
+    "/booking/{booking_id}",
+    tags=["bookings"],
+    status_code=status.HTTP_200_OK,
+    response_model=booking_schema.Booking,
+    dependencies=[Depends(get_db)]
+)
+def get_booking(
+    booking_id: int = Path(
+        ...,
+        gt=0
+    )
+):
+    return booking_service.get_booking(booking_id)
+
+@router.patch(
+    "/{booking_id}/state",
+    tags=["bookings"],
+    status_code=status.HTTP_200_OK,
+    response_model=booking_schema.Booking,
+    dependencies=[Depends(get_db)]
+)
+def update_booking(
+    booking_state: str = Body(...),
+    booking_id: int = Path(
+        ...,
+        gt=0
+    ),
+    #current_user: User = Depends(get_current_user)
+):
+    return booking_service.update_state_booking(booking_state, booking_id)
+
+@router.delete(
+    "/{booking_id}/",
+    tags=["bookings"],
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(get_db)]
+)
+def delete_booking(
+    booking_id: int = Path(
+        ...,
+        gt=0
+    ),
+    #current_user: User = Depends(get_current_user)
+):
+    booking_service.delete_booking(booking_id)
+
+    return {
+        'msg': 'Booking has been deleted successfully'
+    }
